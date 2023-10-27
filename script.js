bcf5f3af52ce9f0070d66b81b6f7276c33195ab5
Farukh <akbfarukh@gmail.com>
2023-10-27 12:31:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+// let str = "Hello World"
+
+// function myFunction(){
+//     let res = str.slice(0,5).toUpperCase()
+//     return res
+// }
+
+// function Farukh(){
+//     let res = str.slice(0,5).toLowerCase()
+//     return res
+// }
+
+// console.log(Farukh())
+// console.log(myFunction())
+
+
+// function myFunction(){
+//     let a = 'Admin'
+//     if (a.toLowerCase() === 'admin'){
+//         return 'Welcome admin'
+//     } else {
+//         return 'Welcome user'
+//     }
+// }
+
+// console.log(myFunction());
+
+function myFunction(){
+    let a = 'Farukh'
+    if (a.toLowerCase() === 'admin'){
+        return 'Welcome admin'
+    } else {
+        return `Welcome ${a}`
+    }
+}
+
+console.log(myFunction());
