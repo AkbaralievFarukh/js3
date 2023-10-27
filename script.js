21627e2548ebfc61b1a187f86343ec0ada28d61e
Farukh <akbfarukh@gmail.com>
2023-10-27 21:19:32 +0600

    modified

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-// let str = "Hello World"
+// Задание 1
 
-// function myFunction(){
-//     let res = str.slice(0,5).toUpperCase()
-//     return res
-// }
-
-// function Farukh(){
-//     let res = str.slice(0,5).toLowerCase()
-//     return res
-// }
-
-// console.log(Farukh())
-// console.log(myFunction())
-
-
-// function myFunction(){
-//     let a = 'Admin'
-//     if (a.toLowerCase() === 'admin'){
-//         return 'Welcome admin'
-//     } else {
-//         return 'Welcome user'
-//     }
-// }
-
-// console.log(myFunction());
-
-// function myFunction(){
-//     let a = 'Farukh'
-//     if (a.toLowerCase() === 'admin'){
-//         return 'Welcome admin'
-//     } else {
-//         return `Welcome ${a}`
-//     }
-// }
-
-// console.log(myFunction());
-
-function myFunction(name){
-    if (name.toLowerCase() === 'admin'){
-        return 'Welcome admin'
+function checkAdmin(user) {
+    if (user.toLowerCase() == 'admin') {
+        return `welcome, ${user}`
     } else {
-        return `Welcome ${name}`
+        return 'not correct user'
     }
 }
 
-console.log(myFunction('Farukh'));
-console.log(myFunction('Shabdan'));
+console.log(checkAdmin('Admin'));
+console.log(checkAdmin('Farukh'));
+
+// Задание 2
+
+function februaryDay(day) {
+    if (day >= 1 && day <=28) {
+        return `today is ${day} of february`
+    } else {
+        return `on february only 28 day, your day is ${day} of another month`
+    }
+}
+
+console.log(februaryDay(13));
+console.log(februaryDay(31));
+
+// Задание 3
+
+function checkEquals (num) {
+    if (num === 50) {
+        return `${num} is equals of 50`
+    } else {
+        return `${num} is not correct`
+    }
+}
+
+console.log(checkEquals(50));
+console.log(checkEquals(1));
+
+// Задание 4
+
+function ageCheker (user, age) {
+    if (age >= 18 && age <= 50) {
+        return `${user} ${age} old, welcome`
+    } else {
+        return `${user} ${age} old, rejected`
+    }
+}
+
+console.log(ageCheker('Farukh', 22));
+console.log(ageCheker('Shabdan', 17));
